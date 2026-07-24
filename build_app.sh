73f4515b2050258f0506786cb3cb35812c36f7db
Kevin FOO <oofnivek@gmail.com>
2026-07-24 23:40:19 +0800

    completed app

--- build_app.sh
+++ build_app.sh
@@ -26,6 +26,7 @@ mkdir -p "${APP_BUNDLE}/Contents/Resources"
 
 cp "${BUILD_DIR}/${APP_NAME}" "${APP_BUNDLE}/Contents/MacOS/${APP_NAME}"
 cp Resources/Info.plist "${APP_BUNDLE}/Contents/Info.plist"
+cp Resources/AppIcon.icns "${APP_BUNDLE}/Contents/Resources/AppIcon.icns"
 
 echo "Signing with \"${SIGNING_IDENTITY}\"..."
 codesign --force --deep --options runtime \
